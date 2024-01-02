b107ba1c343e2503e779d2593ca5e11532a74623
dami <de.jeong@refactoring.kr>
2024-01-02 22:52:55 +0900

    대댓글 구현

--- mysite/views.py
+++ mysite/views.py
@@ -156,7 +156,7 @@ def comment_modify_question(request, comment_id):
         return redirect('mysite:detail', question_id=comment.question.id)
 
     if request.method == "POST":
-        form = CommentForm(request.POST, instance=comment) #덮어쓰기
+        form = CommentForm(request.POST, instance=comment)  # 덮어쓰기
         if form.is_valid():
             comment = form.save(commit=False)
             comment.author = request.user
@@ -164,7 +164,7 @@ def comment_modify_question(request, comment_id):
             comment.save()
             return redirect('mysite:detail', question_id=comment.question.id)
     else:
-        form = CommentForm(instance=comment) #기존내용 보여주기
+        form = CommentForm(instance=comment)  # 기존내용 보여주기
     context = {'form': form}
     return render(request, 'mysite/comment_form.html', context)
 
@@ -179,3 +179,55 @@ def comment_delete_question(request, comment_id):
     else:
         comment.delete()
     return redirect('mysite:detail', question_id=comment.question.id)
+
+
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.answer = answer
+            comment.save()
+            return redirect('mysite:detail', question_id=comment.answer.question.id)  # form안에 comment 기준
+    else:
+        form = CommentForm()
+    context = {'form': form}
+    return render(request, 'mysite/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        messages.error(request, '댓글 수정 권한이 없습니다.')
+        return redirect('mysite:detail', question_id=comment.answer.question.id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.modify_date = timezone.now()
+            comment.save()
+            return redirect('mysite:detail', question_id=comment.answer.question.id) #answer값에 저장하니까 => answer통해서 question접근
+
+    else:
+        form = CommentForm(instance=comment)
+    context = {'form': form}
+    return render(request, 'mysite/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        messages.error(request, '삭제 권한이 없습니다.')
+        return redirect('mysite:detail', question_id=comment.answer.question.id)
+    else:
+        comment.delete()
+    return redirect('mysite:detail', question_id=comment.answer.question.id)
